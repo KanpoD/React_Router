fba0930f3bacc32f4ae43963f628c16c295b5a41
Tom <tom.dardilhac@laposte.net>
2024-04-08 15:21:59 +0200

    path changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Routes, Route, NavLink} from "react-router-dom"
 import Home from "./pages/Home/index.jsx";
 import AddPost from "./pages/AddPost/index.jsx";
 import NavBar from "./component/NavBar/index.jsx";
-import Param from "./pages/Param/index.jsx";
+import PostDetails from './component/PostDetails.jsx/index.jsx';
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
             <Routes>
                 <Route path={'/'} element={<Home/>}/>
                 <Route path={'/add-post'} element={<AddPost/>}/>
-                <Route path={'/param/:id'} element={<Param />} />
+                <Route path={'/post/:id'} element={<PostDetails />} />
                 {/*Gestion des routes non matchée*/}
                 <Route path={'*'} element={<div>Rien ici</div>} />
             </Routes>
